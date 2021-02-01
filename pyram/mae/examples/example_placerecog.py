186ce6b530209e755199b4e45d5bffe283ec46f0
thiagorjes <thiagorjes@outlook.com>
2021-01-31 23:11:48 -0300

    correção em example_placerecog

--- pyram/mae/examples/example_placerecog.py
+++ pyram/mae/examples/example_placerecog.py
@@ -1,6 +1,6 @@
-from mae.vgram.vgram_core import VGRAM
-from mae.vgram.vgram_output import NetworkOutput
-from mae.vgram.vgram_synapse import ConnectionGaussian, ConnectionRandom, ConnectionInput
+from pyram.mae.vgram.vgram_core import VGRAM
+from pyram.mae.vgram.vgram_output import NetworkOutput
+from pyram.mae.vgram.vgram_synapse import ConnectionGaussian, ConnectionRandom, ConnectionInput
 from example_placerecog_utils import LoadDataset, ClearFalseNegatives, EvaluateOutput
 from example_placerecog_config import params
 import numpy as np
@@ -20,14 +20,14 @@ def run_vgram(train_filename, train_path, test_filename, test_path,
                            ]
 
     memory_size, input_size, num_samples = LoadDataset(train_filename, train_path)
-    print 'Train size:', memory_size, input_size, num_samples, filename  
+    print 'Train size:', memory_size, input_size, num_samples, train_filename  
 
-    network.train(memory_size, input_size, num_samples, filename,True)
+    network.train(memory_size, input_size, num_samples, train_filename,True)
 
     memory_size, input_size, num_samples  = LoadDataset(test_filename, test_path)
-    print 'Test size:', memory_size, input_size, num_samples, filename  
+    print 'Test size:', memory_size, input_size, num_samples, test_filename  
     
-    output_data = network.test(memory_size, input_size, num_samples, filename,True)
+    output_data, test_label = network.test(memory_size, input_size, num_samples, test_filename,True)
     
     
     '''the output computed below takes the closest of top 3 most voted, which is closest to the previous'''
@@ -36,9 +36,9 @@ def run_vgram(train_filename, train_path, test_filename, test_path,
     '''the output computed below takes the closest of top 3 most voted, which is closest to ground truth'''
     #pred_label = NetworkOutput.MajorityVoteClosestToExpected(output_data, test_label)
     '''the output computed below takes the most voted and doesn't take into consideration the confidence'''
-    pred_label, confidence = NetworkOutput.MajorityVoteAndConfidence(output_data, test_label.shape[0])
+    pred_label, confidence = NetworkOutput.MajorityVoteAndConfidence(output_data, num_samples)
     #ClearLowConfidence(pred_label, confidence, 0.1)
-    pred_label[0:batch_size] = test_label[0:batch_size] #disregard first results
+    #pred_label[0:batch_size] = test_label[0:batch_size] #disregard first results
     
     result_file = open(result_filename, 'w')
     for frame_radius in xrange(19):
